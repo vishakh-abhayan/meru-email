e84114979e46aacde5f026e12235d889612164c5
Mishalabdullah <github.ijvob@simplelogin.co>
2024-04-20 23:30:05 +0530

    all errors resolved

--- components/ui/textarea.jsx
+++ components/ui/textarea.jsx
@@ -57,11 +57,14 @@ const Textarea = React.forwardRef(({ className, ...props }, ref) => {
   return (
     <div onKeyDown={handleKeyDown} tabIndex={0}>
       <textarea
-        ref={editorRef}
+        // ref={editorRef}
         value={textarea}
         onChange={handleTextareaChange}
-        className="flex h-[350px] w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:ring-offset-gray-950 dark:placeholder:text-gray-400 dark:focus-visible:ring-gray-300"
-        {...props}
+        className={cn(
+          "flex min-h-[350px] w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:ring-offset-gray-950 dark:placeholder:text-gray-400 dark:focus-visible:ring-gray-300",
+          className
+        )}
+        // {...props}
       />
     </div>
   );
